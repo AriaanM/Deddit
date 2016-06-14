28f85eda571bc6a0fa69343968f52177a0063b26
Ariaan Mans <99034779@mydavinci.nl>
2016-06-14 12:09:56 +0200

    Registrationfix
    You can register now

--- application/view/post/index.php
+++ application/view/post/index.php
@@ -7,12 +7,12 @@
 		<h3><a href="create.php"> Make your own post!</a></h3>
         <?php if ($this->post) { ?>
                     <?php foreach($this->post as $key => $value) { ?>
-                    <div class="post">
+                    <a href="#"><div class="post">
                         <tr>
                             <td><h2><?= $value->post_title; ?></h2></td>
-                            <td>By <?= htmlentities($value->post_user); ?></td>
+                            <td>By <?= htmlentities($value->post_author); ?></td>
                         </tr>
-                    </div>
+                    </div></a>
                     <?php } ?>
                 </tbody>
             </table>
